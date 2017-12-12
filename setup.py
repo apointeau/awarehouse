dce9964aaa58d146c9d0dec3eba492dd3712a1f7
Antoine Pointeau <web.pointeau@gmail.com>
2017-12-12 23:45:53 +0100

    Cleanup python package file

--- setup.py
+++ setup.py
@@ -1,29 +1,54 @@
-#! /usr/bin/env python3
+import os
+from codecs import open
 
 from setuptools import setup, find_packages
+
 from awarehouse import __version__
 
-PACKAGE_NAME = "awarehouse"
-
-with open('README.rst') as f:
-    README = f.read()
-
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+with open(os.path.join(here, "requirement.txt")) as f:
+    install_requires = [entry for entry in f]
 
 setup(
-    name=PACKAGE_NAME,
+    name="awarehouse",
     version=__version__,
-    license="GNU GENERAL PUBLIC LICENSE V3",
+
+    description="Awarehouse is a distributed smart file manager",
+    long_description=long_description,
+
     url="https://github.com/apointeau/awarehouse",
     download_url="https://github.com/apointeau/awarehouse.git",
+
     author="Antoine POINTEAU",
     author_email="web.pointeau@gmail.com",
-    description="",
-    long_description=README,
+
+    license="GNU GENERAL PUBLIC LICENSE V3",
+
+    classifiers=[
+        "Development Status :: 1 - Planning",
+
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Desktop Environment :: File Managers",
+
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5"
+    ],
+
+    keywords="file manager smart classifier",
+
     packages=find_packages(),
+
+    install_requires=install_requires,
+
+    extras_require={},
+
     entry_points={
         'console_scripts': [
-            'awarehouse = awarehouse.cli.__main__:main'
+            'awarehouse=awarehouse.cli.__main__:main'
         ]
     },
-    zip_safe=False
 )
